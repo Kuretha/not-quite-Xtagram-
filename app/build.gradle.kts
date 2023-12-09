c3eb8fce69c11507a48006e19efbd8624859a048
Htifano <91378921+Kuretha@users.noreply.github.com>
2023-12-09 13:23:12 +0700

    adding post ui and fix build.gradle.kts (app) dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,13 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.10.0")
+
+    // external libs
+    implementation ("com.hendraanggrian.appcompat:socialview:0.1")
+    implementation ("com.hendraanggrian.appcompat:socialview-autocomplete:0.1")
+    implementation("com.vanniktech:android-image-cropper:4.5.0")
+
+    // firebase libs
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth:22.3.0")
@@ -45,6 +52,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-functions:20.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
